b576fc3eb974ab82efaa5cdb216fad2bef5ea50b
action <noop@github.com>
2024-11-26 03:00:20 +0000

    [actions skip]: Pages 53

--- web/flutter_service_worker.js
+++ web/flutter_service_worker.js
@@ -14,8 +14,8 @@ const RESOURCES = {"flutter.js": "f393d3c16b631f36852323de8e583132",
 "assets/shaders/ink_sparkle.frag": "ecc85a2e95f5e9f53123dcaf8cb9b6ce",
 "assets/AssetManifest.json": "a45f93b9feabdc0bf83002a7c284aea4",
 "assets/AssetManifest.bin.json": "3caa4b2b801a9550e9b06cb5fd938eac",
-"index.html": "9e68592d8eeb3fce73d7eaaca2de4555",
-"/": "9e68592d8eeb3fce73d7eaaca2de4555",
+"index.html": "e1fda7ff244b1e9b8b6d0b26b6aec378",
+"/": "e1fda7ff244b1e9b8b6d0b26b6aec378",
 "manifest.json": "15f73b7e8a8209c2206210b3ac8dea1b",
 "canvaskit/canvaskit.js": "66177750aff65a66cb07bb44b8c6422b",
 "canvaskit/canvaskit.js.symbols": "48c83a2ce573d9692e8d970e288d75f7",
@@ -30,8 +30,8 @@ const RESOURCES = {"flutter.js": "f393d3c16b631f36852323de8e583132",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"version.json": "dfc8c295cbff24909d53c37e6e8ca296",
-"flutter_bootstrap.js": "74ad43332d3f523c5d30632228a5dd05"};
+"version.json": "e7faa388eb28c2e415670f531c317a21",
+"flutter_bootstrap.js": "974727290f9efd554f248e612ba7440f"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
