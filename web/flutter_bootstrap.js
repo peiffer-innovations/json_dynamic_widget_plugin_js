d2481da13c80abd83d03e116c39cd5f7ade64cf0
action <noop@github.com>
2025-04-21 21:18:44 +0000

    [actions skip]: Pages 69

--- web/flutter_bootstrap.js
+++ web/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2685719528"
+    serviceWorkerVersion: "1086585977"
   }
 });
