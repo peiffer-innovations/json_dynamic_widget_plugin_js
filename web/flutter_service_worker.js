f607a1c22cf0301c064275702e248d1a673c916c
action <noop@github.com>
2025-04-22 03:05:10 +0000

    [actions skip]: Pages 70

--- web/flutter_service_worker.js
+++ web/flutter_service_worker.js
@@ -34,7 +34,7 @@ const RESOURCES = {"canvaskit/skwasm.js": "f2ad9363618c5f62e813740099a80e63",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "manifest.json": "0867c3e13649ac4d06fe34b7b3ddce08",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"flutter_bootstrap.js": "e25f8a35702c18f90b80b0240fd61804"};
+"flutter_bootstrap.js": "f45a884e9c7efe998dc3b8ee00559863"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
